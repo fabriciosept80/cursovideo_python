2ac8f57b9ab1c2a19add1768195ce863ccaf4b0c
Fabricio Henriques <fabriciocmh@outlook.com>
2021-08-07 20:02:23 -0300

    novo arquivo do curso

--- Exec0079.py
+++ Exec0079.py
@@ -2,3 +2,17 @@
 #exista la dentro, ele não será adicionado. No final, serão exibidos todos os valores únicos digitados, em ordem
 #crescente.
 
+lista = []
+print()
+print('#'*40)
+print('\033[1;34mTESTE DE LISTA\033[m'.center(50))
+print('#'*40)
+print("ENTRE COM 10 NÚMEROS")
+
+for x in range(1,10):
+    num=int(input("Digite um numero: "))
+    if num in lista:
+        lista.remove(num)
+    lista.append(num)
+    lista.sort()
+print(f'Os número digitados foram: {lista}')
